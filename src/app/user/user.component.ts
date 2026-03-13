6b1d84f4c06abe9cb3b0722175dd4a6e22ed4024
payamd <payamd@users.noreply.github.com>
2026-03-13 12:28:59 -0400

    adding list of users to the application

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users'; 
+const userIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  selectedUser = DUMMY_USERS[userIndex];
+
+
+}
